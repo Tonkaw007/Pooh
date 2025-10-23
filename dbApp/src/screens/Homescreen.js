386b57afd40eb5889b84349f4e98f1cf3cab9e5b
TK^^ <tonkaw81532@gmail.com>
2025-10-23 18:17:29 +0700

    Update
    icon Home

--- dbApp/src/screens/Homescreen.js
+++ dbApp/src/screens/Homescreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { View, Text, StyleSheet, Animated } from 'react-native';
-import Icon from "react-native-vector-icons/FontAwesome5";
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const Homescreen = ({ navigation }) => {
     const fadeAnim = useRef(new Animated.Value(1)).current;
@@ -20,7 +20,7 @@ const Homescreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <View style={styles.iconContainer}>
-                <Icon name="car-alt" size={80} color="black" />
+                <FontAwesome5 name="car-alt" size={80} color="black" />
                 <Text style={styles.pabuText}>POOH</Text>
             </View>
             <Animated.View style={{ opacity: fadeAnim }}>
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        backgroundColor: '#B19CD8', 
+        backgroundColor: '#B19CD8',
     },
     iconContainer: {
         alignItems: 'center',
